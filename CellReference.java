c806b41ff632cd116366ba339ffccd545535714c
ran190 <101316992+ran190@users.noreply.github.com>
2023-12-04 17:47:56 +0100

    Update CellReference.java

--- CellReference.java
+++ CellReference.java
@@ -1,14 +1,8 @@
 public class CellReference implements Node {
     protected String reference;
-    protected Spreadsheet spreadsheet; 
 
     public CellReference(String text) {
         this.reference = text;
-    }
-
-    public CellReference(String text, Spreadsheet spreadsheet) {
-        this.reference = text;
-        this.spreadsheet = spreadsheet;
     }
 
     public String toString() {
@@ -18,7 +12,7 @@ public class CellReference implements Node {
     @Override
     public double getValue() {
         int[] intPlace = Manager.convert(reference);
-        String content = spreadsheet.printCellContent(intPlace[0], intPlace[1]);
+        String content = Spreadsheet.printCellContent(intPlace[0], intPlace[1]);
         try {
             // Attempt to convert content to a number
             return Double.parseDouble(content);
